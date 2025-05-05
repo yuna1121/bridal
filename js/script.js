0ece931fa8ec0e35b8345dbe6e7fa407efd124c0
{yuna} <{scorpio1121.yuuka@gmail.com}>
2025-05-06 08:04:43 +0900

    SPのピクパ完了、PCのピクパ途中

--- js/script.js
+++ js/script.js
@@ -66,42 +66,78 @@ jQuery("#js-drawer-background2").on("click", function (e) {
 
 // swiper
 // class名は自分で決めたクラス名またはIDにする。複数あるときにバッティングする
-const swiper = new Swiper("#js-recent-swiper1", {
+const swiper = new Swiper(".recent1__swiper", {
   // Optional parameters
-  loop: true,
+  // loop: true,
   slidesPerView: "auto",
   spaceBetween: 19,
   breakpoints: {
     768: {
       spaceBetween: 48,
+      centeredSlides: true, // 中央寄せにする
+      initialSlide: 1,
+      
     },
   },
   // Navigation arrows
   navigation: {
-    nextEl: "#js-recent-swiper1-next",
-    prevEl: "#js-recent-swiper1-prev",
+    nextEl: ".recent1__swiper-button-next",
+    prevEl: ".recent1__swiper-button-prev",
   },
 });
 
+
+const cardSwiper = (selector, buttonPrev, buttonNext) => {
+  return new Swiper(selector, { //swiperの名前
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    // centeredSlides: true,
+    768: {
+      centeredSlides: true, // 中央寄せにする
+      // initialSlide: 2,
+      
+    },
+
+    //ナビゲーション
+    navigation: {
+      prevEl: buttonPrev, //戻るボタンのclass
+      nextEl: buttonNext //進むボタンのclass
+    },
+  });
+}
+
+//swiperを生成
+const cardSwiper1 = cardSwiper(".recent2__swiper",  ".recent2__swiper-button-prev", ".recent2__swiper-button-next");
+
+let cardSwiper2 = null;
+window.addEventListener('load', () => {
+  if (window.innerWidth < 768) {
+    cardSwiper2 = cardSwiper(".reports__swiper",  ".reports__swiper-prev", ".reports__swiper-next");
+    swiperStatus = true
+  } else {
+    swiperStatus = false
+  }
+})
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768 && swiperStatus) {
+    cardSwiper2.destroy(false, true)
+    swiperStatus = false
+  } else if (window.innerWidth < 768 && !swiperStatus) {
+    cardSwiper2 = cardSwiper(".reports__swiper",  ".reports__swiper-prev", ".reports__swiper-next");
+    swiperStatus = true
+  }
+})
+
+const cardSwiper3 = cardSwiper(".reports-news__swiper",  ".reports-news__swiper-prev", ".reports-news__swiper-next");
+
+
+
+
 // swiper
 // class名は自分で決めたクラス名またはIDにする。複数あるときにバッティングする
-const swiper2 = new Swiper("#js-recent-swiper2", {
-  // Optional parameters
-  loop: true,
-  slidesPerView: "auto",
-  spaceBetween: 16,
-  // centeredSlides: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: "#js-recent-swiper2-next",
-    prevEl: "#js-recent-swiper2-prev",
-  },
-});
-
-// swiper
-// class名は自分で決めたクラス名またはIDにする。複数あるときにバッティングする
-const swiper3 = new Swiper("#js-about-swiper", {
+const swiper3 = new Swiper(".about__swiper", {
   // Optional parameters
   loop: true,
 
@@ -118,52 +154,12 @@ const swiper3 = new Swiper("#js-about-swiper", {
   },
 });
 
-// swiper
+
+
+// 人気プランナーランキング
 // class名は自分で決めたクラス名またはIDにする。複数あるときにバッティングする
-const swiper4 = new Swiper("#js-reports-swiper", {
-  // Optional parameters
-
-  slidesPerView: "auto",
-  loop: true,
-  spaceBetween: 16,
-  breakpoints: {
-    1085: {
-      loop: false,
-      spaceBetween: 0,
-    },
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: "#js-reports-swiper-next",
-    prevEl: "#js-reports-swiper-prev",
-  },
-});
-
-// swiper
-// class名は自分で決めたクラス名またはIDにする。複数あるときにバッティングする
-const swiper5 = new Swiper("#js-reports-news-swiper", {
-  loop: true,
-
-  slidesPerView: "auto",
-  spaceBetween: 16,
-  breakpoints: {
-    805: {
-      spaceBetween: 32,
-    },
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: "#js-reports-news-swiper-next",
-    prevEl: "#js-reports-news-swiper-prev",
-  },
-});
-
-// swiper
-// class名は自分で決めたクラス名またはIDにする。複数あるときにバッティングする
-const swiper6 = new Swiper("#js-plannerranking-swiper", {
-  loop: true,
+const swiper6 = new Swiper(".plannerranking__swiper", {
+  loop: false,
 
   slidesPerView: "auto",
   spaceBetween: 16,
@@ -175,14 +171,14 @@ const swiper6 = new Swiper("#js-plannerranking-swiper", {
 
   // Navigation arrows
   navigation: {
-    nextEl: "#js-plannerranking-next",
-    prevEl: "#js-plannerranking-prev",
+    nextEl: ".plannerranking__swiper-next",
+    prevEl: ".plannerranking__swiper-prev",
   },
 });
 
 // swiper
 // class名は自分で決めたクラス名またはIDにする。複数あるときにバッティングする
-const swiper7 = new Swiper("#js-plannerranking-new-swiper", {
+const swiper7 = new Swiper(".plannerranking-new-swiper", {
   loop: true,
 
   slidesPerView: "auto",
@@ -195,46 +191,48 @@ const swiper7 = new Swiper("#js-plannerranking-new-swiper", {
 
   // Navigation arrows
   navigation: {
-    nextEl: "#js-plannerranking-new-next",
-    prevEl: "#js-plannerranking-new-prev",
+    nextEl: ".plannerranking-new-next",
+    prevEl: ".plannerranking-new-prev",
   },
 });
 
 // swiper
 // class名は自分で決めたクラス名またはIDにする。複数あるときにバッティングする
-const swiper8 = new Swiper("#js-voice-swiper", {
+const swiper8 = new Swiper(".voice-swiper", {
   loop: true,
 
   slidesPerView: "auto",
   spaceBetween: 16,
   breakpoints: {
-    805: {
+    1085: {
       spaceBetween: 24,
     },
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: "#js-voice-next",
-    prevEl: "#js-voice-prev",
+    nextEl: ".voice-next",
+    prevEl: ".voice-prev",
   },
 });
 
 // swiper
 // class名は自分で決めたクラス名またはIDにする。複数あるときにバッティングする
-const swiper9 = new Swiper("#js-voice-ranking-swiper", {
-  loop: true,
+const swiper9 = new Swiper(".voice-ranking__swiper", {
+  loop: false,
 
   slidesPerView: "auto",
   spaceBetween: 16,
   breakpoints: {
-    805: {
+    768: {
       spaceBetween: 0,
     },
   },
 
   // Navigation arrows
 });
+
+
 
 // function img_alter(img) {
 //   if (img.src.match(/Star\.png/)) {
@@ -269,3 +267,19 @@ jQuery(function () {
     }
   });
 });
+
+//414px 未満は JS で viewport を固定する
+(function () {
+  const viewport = document.querySelector('meta[name="viewport"]');
+  function switchViewport() {
+    const value =
+      window.outerWidth > 414
+        ? 'width=device-width,initial-scale=1'
+        : 'width=414';
+    if (viewport.getAttribute('content') !== value) {
+      viewport.setAttribute('content', value);
+    }
+  }
+  addEventListener('resize', switchViewport, false);
+  switchViewport();
+})();
